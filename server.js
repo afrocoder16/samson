3c56b2124f2e9aca7b96ddb418dc5e7bf18e6b57
samson <samson.nigusse7@gmail.com>
2024-10-22 09:50:14 -0500

    fixed contact me

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
+require('dotenv').config(); // Load environment variables
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const { createClient } = require('@supabase/supabase-js');
+const cors = require('cors'); // Enable CORS if needed for different origins
 
 const app = express();
 
-// Serve static files from the dist directory
+// Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '/')));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // To parse JSON requests
+app.use(cors()); // Allow Cross-Origin requests
+
+// Initialize Supabase client using environment variables
+const supabaseUrl = 'https://ntldpxbxgxcebatayyju.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50bGRweGJ4Z3hjZWJhdGF5eWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2NDQ2MjIsImV4cCI6MjA0MzIyMDYyMn0.XWRaFA0HBYf-hEGvw5miZsnbUiNK3TEBv1qTgDMQSXE';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // API endpoint to handle the form data
 app.post('/contact', async (req, res) => {
     const { name, email, message } = req.body;
 
-    const supabaseUrl = 'https://ntldpxbxgxcebatayyju.supabase.co';
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50bGRweGJ4Z3hjZWJhdGF5eWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2NDQ2MjIsImV4cCI6MjA0MzIyMDYyMn0.XWRaFA0HBYf-hEGvw5miZsnbUiNK3TEBv1qTgDMQSXE';
-    const supabase = createClient(supabaseUrl, supabaseKey);
-    
-
+    // Insert form data into Supabase
     const { data, error } = await supabase
         .from('portfolioDB')
         .insert([{ name, email, message }]);
